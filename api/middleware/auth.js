a0bf39a2189d1363815c14b4684406f9baaec2bd
João Azevedo <azevedojoao05@gmail.com>
2026-04-11 18:27:15 +0100

    comentei a parte da autenticacao nao esta direito

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -1,24 +1,24 @@
-const jwt = require('jsonwebtoken');
-const SECRET = process.env.JWT_SECRET || 'segredo';
+// const jwt = require('jsonwebtoken');
+// const SECRET = process.env.JWT_SECRET || 'segredo';
 
-function authenticate(req, res, next) {
-    const token = req.headers.authorization?.split(' ')[1]; // Bearer <token>
-    if (!token) return res.status(401).json({ erro: 'Token em falta' });
+// function authenticate(req, res, next) {
+//     const token = req.headers.authorization?.split(' ')[1]; // Bearer <token>
+//     if (!token) return res.status(401).json({ erro: 'Token em falta' });
 
-    try {
-        req.user = jwt.verify(token, SECRET);
-        next();
-    } catch (err) {
-        res.status(401).json({ erro: 'Token inválido ou expirado' });
-    }
-}
+//     try {
+//         req.user = jwt.verify(token, SECRET);
+//         next();
+//     } catch (err) {
+//         res.status(401).json({ erro: 'Token inválido ou expirado' });
+//     }
+// }
 
-function authorize(...roles) {
-    return (req, res, next) => {
-        if (!roles.includes(req.user.role))
-            return res.status(403).json({ erro: 'Sem permissão' });
-        next();
-    };
-}
+// function authorize(...roles) {
+//     return (req, res, next) => {
+//         if (!roles.includes(req.user.role))
+//             return res.status(403).json({ erro: 'Sem permissão' });
+//         next();
+//     };
+// }
 
-module.exports = { authenticate, authorize };
+// module.exports = { authenticate, authorize };
